987dec742852dc54f8c00cfc1d5596c829af1be0
glogjones <138886032+glogjones@users.noreply.github.com>
2024-12-02 16:00:07 +0000

    Update vision_transformer.py
    changed inchans variable in PatchEmbed class from 3 to 1 for greyscale fits cutouts.

--- src/models/vision_transformer.py
+++ src/models/vision_transformer.py
@@ -174,7 +174,7 @@ class Block(nn.Module):
 class PatchEmbed(nn.Module):
     """ Image to Patch Embedding
     """
-    def __init__(self, img_size=224, patch_size=16, in_chans=3, embed_dim=768):
+    def __init__(self, img_size=224, patch_size=16, in_chans=1, embed_dim=768):
         super().__init__()
         num_patches = (img_size // patch_size) * (img_size // patch_size)
         self.img_size = img_size
